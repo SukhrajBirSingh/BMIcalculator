18467d7323267f507f8a11a4f535c2ea8e716085
Sukhraj Bir Singh <sgosal15@gmail.com>
2022-02-03 23:18:43 -0700

    initial commit

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Admin on 2021-09-02.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+
+struct BMI {
+    let value : Float
+    let advice : String
+    let color : UIColor
+    
+}
